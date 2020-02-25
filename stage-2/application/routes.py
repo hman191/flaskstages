3b50de5659a9f532d6d645157ccb9aa8a53a504c
Ubuntu <flasktesth@flasktest.qxc21f5crpdetmu44db2hobv4b.zx.internal.cloudapp.net>
2020-02-25 15:20:13 +0000

    project from tutorial stage 2 with my changes

---
+++ stage-2/application/routes.py
@@ -0,0 +1,19 @@
+from application import app
+from flask import render_template
+from application.models import Posts
+
+@app.route('/')
+@app.route('/home')
+def home():
+    postData = Posts.query.first()
+    return render_template('home.html', title='Home', post=postData)
+@app.route('/about')
+def about():
+    return render_template('about.html', title='about')
+@app.route('/register')
+def register():
+    return render_template('register.html', title='Register')
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Login')
+
